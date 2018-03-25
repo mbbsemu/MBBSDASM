e20eb21920343253cfc41378f46b488c938119cd
Eric P. Nusbaum <eric@enusbaum.com>
2018-03-24 23:38:14 -0400

    Enhanced tracking of global and local variables

--- MBBSDASM/Analysis/Artifacts/TrackedVariable.cs
+++ MBBSDASM/Analysis/Artifacts/TrackedVariable.cs
@@ -1,11 +1,19 @@
-﻿namespace MBBSDASM.Analysis.Artifacts
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MBBSDASM.Analysis.Artifacts
 {
     public class TrackedVariable
     {
-        public ushort Segment { get; set; }
-        public ulong Offset { get; set; }
-        public ushort Address { get; set; }
         public string Name { get; set; }
-        public string Comment { get; set; }
+        public ushort MemoryAddress { get; set; }
+        
+        /// <summary>
+        ///     We only track values to identify bool vs numeric
+        ///     (sum(values) == 1 && count(values) == 2) == bool
+        /// </summary>
+        public List<int> Values { get; set; }
+
+        public bool isBool => Values.Count >= 2 && Values.Max() <= 1;
     }
 }
